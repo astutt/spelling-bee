66b43b1d94789ee04a3117a0c5a1df2442c1c3f4
Alex Stutt <alexandra.stutt@gmail.com>
2022-09-22 23:17:47 -0400

    initial framework

---
+++ spelling_bee.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# The `@streamlit.cache` decorate ensures that this only needs to happen once
+# Function to get list of words
+@st.cache
+def get_dict()
+
+    return word_list
+
+# GameState class stores all game attribute as streamlit state
+
+
+# New game button
+
+
+# Put the rest of the game logic down here
